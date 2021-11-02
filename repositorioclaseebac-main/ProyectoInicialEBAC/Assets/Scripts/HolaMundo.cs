114a75cb66f538f7ed05966018dd2778a43af38d
Paolo Mar <PaoloMar_@hotmail.com>
2021-11-02 10:37:13 -0600

    Se creo todo lo de unity
    Todo Piola

---
+++ repositorioclaseebac-main/ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Algo paso");
+        Debug.Log("Todo Piola");
+        Debug.LogWarning("Esto no es un error");
+        Debug.LogError("Esto esta mal xd");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

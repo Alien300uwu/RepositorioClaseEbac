4fd2e8a0cc089dbf55b23ca9baa4096b2c0d712d
Paolo Mar <PaoloMar_@hotmail.com>
2021-11-11 17:34:13 -0600

    Actualización
    Se actualizó el software de Unity

--- repositorioclaseebac-main/ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
+++ repositorioclaseebac-main/ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+
+    int x;
     // Start is called before the first frame update
     void Start()
     {
+        x = 0;
         print("Algo paso");
-        Debug.Log("Todo Piola");
+        
         Debug.LogWarning("Esto no es un error");
         Debug.LogError("Esto esta mal xd");
     }
@@ -16,6 +19,7 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        x = x + 1;
+        Debug.Log(x);
     }
 }

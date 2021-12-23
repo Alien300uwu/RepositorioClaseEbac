463988541c2d32c4191c80794ed3584099726f05
Paolo Mar <PaoloMar_@hotmail.com>
2021-12-22 20:30:32 -0600

    Modulo 9 Tarea

--- repositorioclaseebac-main/ProyectoInicialEBAC/Assets/Modulo 9/ejercicios_De_Ciclos_y_Arreglos.cs
+++ repositorioclaseebac-main/ProyectoInicialEBAC/Assets/Modulo 9/ejercicios_De_Ciclos_y_Arreglos.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class ejercicios_De_Ciclos_y_Arreglos : MonoBehaviour
 {
 
-    
 
+    
     // Start is called before the first frame update
     void Start()
     {
         //Ejercicio 1
+        bool Juntadoinador = true;        
         int[] arre_1 = new int[0];
-        int[] arre_2 = new int[4];
+        int[] arre_2 = new int[0];
         int[] arre_3 = new int[0];
+        do
+        {
+            arre_1 = new int[Random.Range(0, 10)];
+            arre_2 = new int[Random.Range(0, 10)];
+            arre_1.ToString
+            Debug.Log(arre_1);
+            Debug.Log(arre_2);
+            Juntadoinador = false;
+        } while(Juntadoinador);
 
 
         //Ejercicio 2
